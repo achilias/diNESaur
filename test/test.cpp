8c26f1f2ce5ba5dc2594bf2c523277e0c78540c1
MariosAchilias <marios.axilias@protonmail.com>
2025-04-28 17:34:28 +0300

    Add helper functions for opcode unit tests and parsing json test cases

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,53 @@
 #include <gtest/gtest.h>
 
-TEST(CPU_Tests, dummy) {
+#include <fstream>
+#include <nlohmann/json.hpp>
+#include "test_cpu.h"
 
+using json = nlohmann::json;
+
+void TestCPU::mem_locs_set(std::vector<std::tuple<uint16_t, uint8_t>> list) {
+    for (auto iter = list.begin(); iter != list.end(); iter++) {
+        mem.write_byte(std::get<0>(*iter), std::get<1>(*iter));
+    }
+}
+
+void TestCPU::mem_locs_test(std::vector<std::tuple<uint16_t, uint8_t>> list) {
+    for (auto iter = list.begin(); iter != list.end(); iter++) {
+        uint8_t byte = mem.read_byte(std::get<0>(*iter));
+        EXPECT_EQ(byte, std::get<1>(*iter));
+    }
+}
+
+void TestCPU::set_state(uint16_t pc, uint8_t sp, uint8_t accum, uint8_t reg_x, uint8_t reg_y, uint8_t sr) {
+    this->pc = pc; this->sp = sp; this->accum = accum; this->reg_x = reg_x; this->reg_y = reg_y; this->sr = sr;
+};
+
+void TestCPU::test_state(uint16_t pc, uint8_t sp, uint8_t accum, uint8_t reg_x, uint8_t reg_y, uint8_t sr) {
+    EXPECT_EQ(this->pc, pc);
+    EXPECT_EQ(this->sp, sp);
+    EXPECT_EQ(this->accum, accum);
+    EXPECT_EQ(this->reg_x, reg_x);
+    EXPECT_EQ(this->reg_y, reg_y);
+    EXPECT_EQ(this->sr, sr);
+};
+
+void TestCPU::test_opcode(std::string opcode) {
+    std::ifstream f("../test/json/" + opcode + "_test.json");
+    json data = json::parse(f);
+    for(auto test_case = data.begin(); test_case != data.end(); test_case++) {
+        auto init = (*test_case)["initial"];
+        set_state(init["pc"], init["p"], init["a"], init["x"], init["y"], init["s"]);
+        mem_locs_set(init["ram"]);
+
+        auto final = (*test_case)["final"];
+        test_state(final["pc"], final["p"], final["a"], final["x"], final["y"], final["s"]);
+        mem_locs_test(final["ram"]);
+
+        std::cout << (*test_case)["name"] << std::endl;
+    }
+}
+
+TEST(CPU_Tests, dummy) {
+    TestCPU cpu;
 }
--- test/test_cpu.h
+++ test/test_cpu.h
@@ -2,11 +2,16 @@
 #define TEST_CPU_H
 
 #include "cpu.h"
+#include <vector>
 
 class TestCPU : public CPU {
 public:
     TestCPU() : CPU(false) {};
-
+    void test_opcode(std::string opcode);
+    void set_state(uint16_t pc, uint8_t sp, uint8_t accum, uint8_t reg_x, uint8_t reg_y, uint8_t sr);
+    void test_state(uint16_t pc, uint8_t sp, uint8_t accum, uint8_t reg_x, uint8_t reg_y, uint8_t sr);
+    void mem_locs_set(std::vector<std::tuple<uint16_t, uint8_t>> list);
+    void mem_locs_test(std::vector<std::tuple<uint16_t, uint8_t>> list);
 };
 
 #endif

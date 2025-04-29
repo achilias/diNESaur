db735c61a87f8d2b9a91229aad0515d20768c5a8
MariosAchilias <marios.axilias@protonmail.com>
2025-04-29 03:08:40 +0300

    Fix read_two_bytes (was big endian)

--- include/memory.h
+++ include/memory.h
@@ -7,7 +7,7 @@ class Memory {
 public:
     explicit Memory(std::size_t mem_size) : buffer(mem_size) {};
     inline uint8_t read_byte(uint16_t addr) const { return buffer[mirror(addr)]; };
-    inline uint16_t read_two_bytes(uint16_t addr) const { return ((uint16_t) buffer[mirror(addr)]) << 8 | buffer[mirror(addr) + 1]; };
+    inline uint16_t read_two_bytes(uint16_t addr) const { return ((uint16_t) buffer[mirror(addr + 1)]) << 8 | buffer[mirror(addr)]; };
     inline void write_byte(uint16_t addr, uint8_t val) { buffer[mirror(addr)] = val; };
     inline void write_two_bytes(uint16_t addr, uint16_t val) { write_byte(mirror(addr), (uint8_t) (val & 0xFF)); write_byte(mirror(addr) + 1, (uint8_t) (val >> 8)); };
     inline bool in_range(uint16_t addr, uint16_t start, uint16_t end) const {return addr >= start && addr <= end;};

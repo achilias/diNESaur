95e0d327561629781abc69f3ebd98cfc2b87227f
MariosAchilias <marios.axilias@protonmail.com>
2025-07-13 23:56:29 +0300

    Add left, right buttons

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -56,6 +56,8 @@ bool GraphicsContext::update(Controller& ctrl) {
             case SDLK_X: idx = static_cast<int>(Ctrl_State::START); break;
             case SDLK_DOWN: idx = static_cast<int>(Ctrl_State::DOWN); break;
             case SDLK_UP: idx = static_cast<int>(Ctrl_State::UP); break;
+            case SDLK_LEFT: idx = static_cast<int>(Ctrl_State::LEFT); break;
+            case SDLK_RIGHT: idx = static_cast<int>(Ctrl_State::RIGHT); break;
             case SDLK_A: idx = static_cast<int>(Ctrl_State::A); break;
             case SDLK_B: idx = static_cast<int>(Ctrl_State::B); break;
         }

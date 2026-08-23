2db3552e6e60a3f4e80de61cb102e16fa449f161
Marios Achilias <marios@achilias.gr>
2026-08-23 03:32:39 +0300

    refactor: move display code from main to separate file

---
+++ src/display.cpp
@@ -0,0 +1,56 @@
+#include <SDL3/SDL.h>
+
+SDL_Window* window;
+SDL_Renderer* renderer;
+SDL_Texture* texture;
+uint32_t* framebuffer;
+const int window_width = 1024;
+const int window_height = 960;
+
+#define SCREEN_WIDTH 256
+#define SCREEN_HEIGHT 240
+
+void display_init() {
+    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
+
+    framebuffer = new uint32_t[window_width * window_height];
+    window = SDL_CreateWindow("DiNESaur", window_width, window_height, 0);
+    renderer = SDL_CreateRenderer(window, nullptr);
+    texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, window_width, window_height);
+}
+
+void display_finish() {
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
+static void draw_rect(int x, int y, int rect_width, int rect_height, uint32_t colour, uint32_t *buf) {
+    for (int x_ = x; x_ < x + rect_width; x_++)
+        for (int y_ = y; y_ < y + rect_height; y_++)
+            buf[y_ * window_width + x_] = colour;
+}
+
+void render(uint64_t ticks, uint32_t *buffer) {
+    int rect_width = window_width / SCREEN_WIDTH;
+    int rect_height = window_height / SCREEN_HEIGHT;
+    for (int i = 0; i < SCREEN_WIDTH; i ++)
+        for (int j = 0; j < SCREEN_HEIGHT; j++)
+            draw_rect(i * rect_width, j * rect_height, rect_width, rect_height, buffer[j * SCREEN_WIDTH + i], framebuffer);
+}
+
+void draw() {
+    char *pixels;
+    int row_sz;
+    SDL_LockTexture(texture, nullptr, (void**) &pixels, &row_sz);
+
+    for (int i = 0, sp = 0, dp = 0; i < window_height; i++, dp += window_width, sp += row_sz)
+        memcpy(pixels + sp, framebuffer + dp, window_width * 4); // 4 bytes per pixel
+
+    SDL_UnlockTexture(texture);
+    SDL_SetRenderDrawColorFloat(renderer, 0.45f, 0.55f, 0.60f, 1.00f);
+    SDL_RenderClear(renderer);
+    SDL_RenderTexture(renderer, texture, nullptr, nullptr);
+    SDL_RenderPresent(renderer);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,6 @@
 
 #include <SDL3/SDL.h>
 
-#define SCREEN_WIDTH 256
-#define SCREEN_HEIGHT 240
-#define FILE_PATH_MAX_LENGTH 1024
-
-const int window_width = 1024;
-const int window_height = 960;
-uint32_t* framebuffer;
-SDL_Window* window;
-SDL_Renderer* renderer;
-SDL_Texture* texture;
 void display_init();
 void display_finish();
 void draw();
@@ -64,20 +54,6 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-void draw() {
-    char *pixels;
-    int row_sz;
-    SDL_LockTexture(texture, nullptr, (void**) &pixels, &row_sz);
-
-    for (int i = 0, sp = 0, dp = 0; i < window_height; i++, dp += window_width, sp += row_sz)
-        memcpy(pixels + sp, framebuffer + dp, window_width * 4); // 4 bytes per pixel
-
-    SDL_UnlockTexture(texture);
-    SDL_SetRenderDrawColorFloat(renderer, 0.45f, 0.55f, 0.60f, 1.00f);
-    SDL_RenderClear(renderer);
-    SDL_RenderTexture(renderer, texture, nullptr, nullptr);
-    SDL_RenderPresent(renderer);
-}
 
 bool update(Controller& ctrl) {
     SDL_Event e;
@@ -91,10 +67,6 @@ bool update(Controller& ctrl) {
         if (e.type == SDL_EVENT_KEY_DOWN || e.type == SDL_EVENT_KEY_UP) {
             int idx;
             switch (e.key.key) {
-                case SDLK_F1:
-                    // TODO: ROM selection UI
-                    return true;
-                // TODO: f2 for control config menu etc
                 case SDLK_Z:
                     idx = static_cast<int>(Ctrl_State::SELECT);
                     break;
@@ -127,34 +99,4 @@ bool update(Controller& ctrl) {
     }
 
     return true;
-}
-
-void display_init() {
-    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
-
-    framebuffer = new uint32_t[window_width * window_height];
-    window = SDL_CreateWindow("DiNESaur", window_width, window_height, 0);
-    renderer = SDL_CreateRenderer(window, nullptr);
-    texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, window_width, window_height);
-}
-
-void display_finish() {
-    SDL_DestroyTexture(texture);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-}
-
-static void draw_rect(int x, int y, int rect_width, int rect_height, uint32_t colour, uint32_t *buf) {
-    for (int x_ = x; x_ < x + rect_width; x_++)
-        for (int y_ = y; y_ < y + rect_height; y_++)
-            buf[y_ * window_width + x_] = colour;
-}
-
-void render(uint64_t ticks, uint32_t *buffer) {
-    int rect_width = window_width / SCREEN_WIDTH;
-    int rect_height = window_height / SCREEN_HEIGHT;
-    for (int i = 0; i < SCREEN_WIDTH; i ++)
-        for (int j = 0; j < SCREEN_HEIGHT; j++)
-            draw_rect(i * rect_width, j * rect_height, rect_width, rect_height, buffer[j * SCREEN_WIDTH + i], framebuffer);
 }

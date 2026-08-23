556fe1a65472c45213b1bdde4ac3345de07a31b0
Marios Achilias <marios@achilias.gr>
2026-08-23 20:15:17 +0300

    fix: TestCPU initialization

--- test/test_cpu.h
+++ test/test_cpu.h
@@ -6,12 +6,22 @@
 
 class TestCPU : public CPU {
 public:
-    TestCPU() : CPU(*new Bus(new ROM(), new Controller())) { bus.map_memory_nes = false; };
+    TestCPU() : CPU(*make_test_bus()) {};
     void test_opcode(std::string opcode);
     void set_state(uint16_t pc, uint8_t sp, uint8_t accum, uint8_t reg_x, uint8_t reg_y, uint8_t sr);
     void test_state(uint16_t pc, uint8_t sp, uint8_t accum, uint8_t reg_x, uint8_t reg_y, uint8_t sr);
     void mem_locs_set(std::vector<std::tuple<uint16_t, uint8_t>> list);
     void mem_locs_test(std::vector<std::tuple<uint16_t, uint8_t>> list);
+
+private:
+    // Constructs a Bus with map_memory_nes already disabled, before CPU's constructor
+    // (which resets the CPU by reading through the bus) ever runs.
+    static Bus *make_test_bus() {
+        Bus *bus = new Bus();
+        bus_init(bus, new ROM(), new Controller());
+        bus->map_memory_nes = false;
+        return bus;
+    }
 };
 
 #endif

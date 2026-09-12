4afdb0027e69b2cf8dd8c34c24e77f290ae75ca2
Marios Achilias <marios@achilias.gr>
2026-09-12 15:08:10 +0300

    refactor: clean up CPU module

--- src/core/cpu.cpp
+++ src/core/cpu.cpp
@@ -1,6 +1,22 @@
 #include "cpu.h"
 #include "nes.h"
 
+#define STACK_BASE 0x100
+
+enum class AddressingMode {
+    accumulator,
+    immediate,
+    zero_page,
+    zero_page_idx_x,
+    zero_page_idx_y,
+    absolute,
+    absolute_idx_x,
+    absolute_idx_y,
+    indirect,
+    indirect_idx_x,
+    indirect_idx_y,
+};
+
 // TODO: implement cycle penalties for page crosses and taken branches
 
 static bool in_range(uint16_t addr, uint16_t start, uint16_t end) {
@@ -83,8 +99,8 @@ void cpu_write_two_bytes(CPU *cpu, uint16_t addr, uint16_t val) { cpu_write_byte
 
 void CPU::handle_nmi() {
     nes->nmi = false;
-    cpu_write_byte(this, stack_base + sp--, flags | 0x20);
-    cpu_write_two_bytes(this, stack_base + sp, pc);
+    cpu_write_byte(this, STACK_BASE + sp--, flags | 0x20);
+    cpu_write_two_bytes(this, STACK_BASE + sp, pc);
     sp -= 2; 
     pc = cpu_read_two_bytes(this, 0xfffa);
 }
@@ -229,9 +245,9 @@ void bpl(CPU *cpu) {
 }
 
 void brk(CPU *cpu) {
-	cpu_write_byte(cpu, cpu->stack_base + cpu->sp--, ++cpu->pc >> 8);
-	cpu_write_byte(cpu, cpu->stack_base + cpu->sp--, cpu->pc & 0xff);
-	cpu_write_byte(cpu, cpu->stack_base + cpu->sp--, cpu->flags | 0x30); // break flag and extra bit (bits 4 & 5) should always be set: 0x30 = 00110000
+	cpu_write_byte(cpu, STACK_BASE + cpu->sp--, ++cpu->pc >> 8);
+	cpu_write_byte(cpu, STACK_BASE + cpu->sp--, cpu->pc & 0xff);
+	cpu_write_byte(cpu, STACK_BASE + cpu->sp--, cpu->flags | 0x30); // break flag and extra bit (bits 4 & 5) should always be set: 0x30 = 00110000
 	cpu->set_disable_interrupt(1);
 	cpu->pc = cpu_read_two_bytes(cpu, 0xfffe); // address of irq interrupt handler
 }
@@ -344,8 +360,8 @@ void jmp(CPU *cpu, AddressingMode addr_mode) {
 
 void jsr(CPU *cpu) {
 	uint16_t addr = cpu_read_two_bytes(cpu, cpu->pc++);
-	cpu_write_byte(cpu, cpu->stack_base + cpu->sp--, cpu->pc >> 8);
-	cpu_write_byte(cpu, cpu->stack_base + cpu->sp--, cpu->pc & 0xff);
+	cpu_write_byte(cpu, STACK_BASE + cpu->sp--, cpu->pc >> 8);
+	cpu_write_byte(cpu, STACK_BASE + cpu->sp--, cpu->pc & 0xff);
 	cpu->pc = addr;
 }
 
@@ -391,22 +407,22 @@ void ora(CPU *cpu, AddressingMode addr_mode) {
 }
 
 void pha(CPU *cpu) {
-	cpu_write_byte(cpu, cpu->stack_base + cpu->sp--, cpu->accum);
+	cpu_write_byte(cpu, STACK_BASE + cpu->sp--, cpu->accum);
 }
 
 void php(CPU *cpu) {
-	cpu_write_byte(cpu, cpu->stack_base + cpu->sp--, cpu->flags | 0x30);
+	cpu_write_byte(cpu, STACK_BASE + cpu->sp--, cpu->flags | 0x30);
 }
 
 void pla(CPU *cpu) {
-	cpu->accum = cpu_read_byte(cpu, cpu->stack_base + ++cpu->sp);
+	cpu->accum = cpu_read_byte(cpu, STACK_BASE + ++cpu->sp);
 	cpu->set_zero(cpu->accum == 0);
 	cpu->set_negative(cpu->accum & 0x80);
 }
 
 void plp(CPU *cpu) {
 	// ignore break flag (bit 4): 0xef = 11101111 and set extra bit (bit 5): 0x20 = 00100000
-	cpu->flags = 0x20 | (cpu_read_byte(cpu, cpu->stack_base + ++cpu->sp) & 0xef);
+	cpu->flags = 0x20 | (cpu_read_byte(cpu, STACK_BASE + ++cpu->sp) & 0xef);
 }
 
 void rol(CPU *cpu, AddressingMode addr_mode) {
@@ -446,15 +462,15 @@ void ror(CPU *cpu, AddressingMode addr_mode) {
 }
 
 void rti(CPU *cpu) {
-	cpu->flags = 0x20 | (cpu_read_byte(cpu, cpu->stack_base + ++cpu->sp) & 0xef);
-    uint8_t pcl = cpu_read_byte(cpu, cpu->stack_base + ++cpu->sp);
-    uint8_t pch = cpu_read_byte(cpu, cpu->stack_base + ++cpu->sp);
+	cpu->flags = 0x20 | (cpu_read_byte(cpu, STACK_BASE + ++cpu->sp) & 0xef);
+    uint8_t pcl = cpu_read_byte(cpu, STACK_BASE + ++cpu->sp);
+    uint8_t pch = cpu_read_byte(cpu, STACK_BASE + ++cpu->sp);
 	cpu->pc = (((uint16_t) pch) << 8) | pcl;
 }
 
 void rts(CPU *cpu) {
-    uint8_t pcl = cpu_read_byte(cpu, cpu->stack_base + ++cpu->sp);
-    uint8_t pch = cpu_read_byte(cpu, cpu->stack_base + ++cpu->sp);
+    uint8_t pcl = cpu_read_byte(cpu, STACK_BASE + ++cpu->sp);
+    uint8_t pch = cpu_read_byte(cpu, STACK_BASE + ++cpu->sp);
     cpu->pc = ((((uint16_t) pch) << 8) | pcl) + 1;
 }
 
--- src/core/cpu.h
+++ src/core/cpu.h
@@ -8,22 +8,8 @@
 
 struct NES;
 
-enum class AddressingMode {
-    accumulator,
-    immediate,
-    zero_page,
-    zero_page_idx_x,
-    zero_page_idx_y,
-    absolute,
-    absolute_idx_x,
-    absolute_idx_y,
-    indirect,
-    indirect_idx_x,
-    indirect_idx_y,
-};
 
-class CPU {
-public:
+struct CPU {
     size_t execute_instr();
     void handle_nmi();
     NES *nes;
@@ -32,7 +18,6 @@ public:
      * TODO: this is a temporary runtime flag; move it to a compile-time
      * macro gated on the test build instead, and drop the field entirely. */
     bool standalone { false };
-    static const uint16_t stack_base = 0x100;
     uint8_t sp { 0xff };
     uint16_t pc { 0 };
     uint8_t accum { 0 };
@@ -41,23 +26,19 @@ public:
     uint8_t flags { 0 };
     std::array<uint8_t, RAM_SIZE> ram {};
 
-    inline void set_carry(bool cond) {flags = cond ? flags | 0x1 : flags & ~0x1;}
-    inline bool get_carry() const {return flags & 0x1;}
-    inline void set_zero(bool cond) {flags = cond ? flags | 0x2 : flags & ~0x2;}
-    inline bool get_zero() const {return flags & 0x2;}
-    inline void set_disable_interrupt(bool cond) {flags = cond ? flags | 0x4 : flags & ~0x4;}
-    inline bool get_disable_interrupt() const {return flags & 0x4;}
-    inline void set_overflow(bool cond) {flags = cond ? flags | 0x40 : flags & ~0x40;};
-    inline void set_decimal(bool cond) {flags = cond ? flags | 0x8 : flags & ~0x8;}
-    inline bool get_decimal() const {return flags & 0x8;}
-    inline bool get_overflow() const {return flags & 0x40;}
-    inline void set_negative(bool cond) {flags = cond ? flags | 0x80 : flags & ~0x80;};
-    inline bool get_negative() const {return flags & 0x80;}
+    void set_carry(bool cond) {flags = cond ? flags | 0x1 : flags & ~0x1;}
+    bool get_carry() const {return flags & 0x1;}
+    void set_zero(bool cond) {flags = cond ? flags | 0x2 : flags & ~0x2;}
+    bool get_zero() const {return flags & 0x2;}
+    void set_disable_interrupt(bool cond) {flags = cond ? flags | 0x4 : flags & ~0x4;}
+    bool get_disable_interrupt() const {return flags & 0x4;}
+    void set_overflow(bool cond) {flags = cond ? flags | 0x40 : flags & ~0x40;};
+    void set_decimal(bool cond) {flags = cond ? flags | 0x8 : flags & ~0x8;}
+    bool get_decimal() const {return flags & 0x8;}
+    bool get_overflow() const {return flags & 0x40;}
+    void set_negative(bool cond) {flags = cond ? flags | 0x80 : flags & ~0x80;};
+    bool get_negative() const {return flags & 0x80;}
 };
 
 void cpu_reset(CPU *cpu);
 void cpu_init(CPU *cpu, NES *nes);
-uint8_t cpu_read_byte(CPU *cpu, uint16_t addr);
-uint16_t cpu_read_two_bytes(CPU *cpu, uint16_t addr);
-void cpu_write_byte(CPU *cpu, uint16_t addr, uint8_t val);
-void cpu_write_two_bytes(CPU *cpu, uint16_t addr, uint16_t val);
